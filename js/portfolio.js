87be26e55ef0101921caa19b9d0e5448e1c5351b
May Ewers <mestipul@gmail.com>
2019-06-03 23:20:03 -0400

    successful slick

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,8 +1,8 @@
-
-$('#fade').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear'
+$(function(){
+  $('#fade').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
   });
+})
